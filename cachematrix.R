077623bc6d467a36580493bdcf4da4b7289cd3de
aeschreiner <tony.schreiner@gmail.com>
2014-11-23 13:42:17 -0500

    Initial adaptation from example file. Changed function compute and cache matrix inverse rather than vector mean

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Pair of functions that create a list structure to enable caching of a
+##  computed matrix inverse
 
-## Write a short comment describing this function
+## Create a list object that stores and accesses an inverse to the supplied
+##  matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    mi <- NULL
+    set <- function(y) {
+        x <<- y
+        mi <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inv) mi <<- inv
+    getinv <- function() mi
+    list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## return the matrix inverse from the given matrix structure
+##  return the cached value of the inverse if found, otherwise compute
+##  the inverse and return it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # check if the inverse matrix is cached, if so return it
+    mi <- x$getinv()
+    if (!is.null(mi)) {
+        message('getting cached inverse')
+        return(mi)
+    }
+    # compute the matrix inverse, cache it, then return it
+    data <- x$get()
+    mi <- solve(data, ...)
+    x$setinv(mi)
+    mi
 }
